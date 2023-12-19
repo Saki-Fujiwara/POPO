16a7871651dfab76dac2647a25aa039c852c2c3e
Saki-Fujiwara <saki.0902s.f@gmail.com>
2023-12-19 15:53:03 +0900

    投稿作成画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,10 +8,11 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                <a href="/posts/{{ $post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
+                <a href="/posts/{{ $post->id}}"></a><h2 class='title'>{{ $post->title }}</h2><>
                 <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
